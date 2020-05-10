db8d4d1e049c7134d090a77eed52ad2eb8af211b
Santiago Ibarzabal <santiagoibarzabal91@gmail.com>
2020-05-10 13:59:29 -0300

    Fix: Correccion de ortografia - SEO en head.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
     <meta name="keywords" content="Propiedades, inmuebles, casas, departamentos, barrios cerrados, alquiler, venta, compra, alquiler temporario, argentina. Casas y Departamentos, inmuebles, propiedades, Venta y Alquiler, Argentina">
     <meta property="og:locale" content="es_AR" />
     <meta property="og:type" content="website" />
-    <meta property="og:title" content="Martīn Ibarzábal : Alquiler y Venta de propiedades &amp; Mercado Inmobiliario, propiedades en venta y alquiler. Encontrá la propiedad que estás buscando" />
+    <meta property="og:title" content="Martín Ibarzábal : Alquiler y Venta de propiedades &amp; Mercado Inmobiliario, propiedades en venta y alquiler. Encontrá la propiedad que estás buscando" />
     <meta property="og:description" content="Ibarzabal Servicios Inmobiliarios puede ayudarlo su búsqueda o necesidad inmobiliaria, ya sea para comprar, alquilar o vender una propiedad. Para encontrar la solución que necesita contáctese con Ibarzabal Servicios Inmobiliarios hoy para encontrar lo qué está buscando" />
     <meta property="og:url" content="https://www.ibarzabal.com" />
     <meta property="og:image" content="{{asset('img/logo.png')}}" />
