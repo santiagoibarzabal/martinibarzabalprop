bf160a6a341d3e2bda22a8573ef99af00e5dfdf6
Santiago Ibarzabal <santiagoibarzabal91@gmail.com>
2020-05-11 08:51:53 -0300

    Fix: Correccion de validaciones y mensajes error.

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Las contraseñas deben ser de por lo menos 8 caracteres y que la confirmación coincida',
+    'reset' => 'Se reseteó tu contraseña!',
+    'sent' => 'Te enviamos por mail un link de reactivación de contraseña!',
+    'token' => 'Este token de reinicio de contraseña es inválido.',
+    'user' => "No podemos encontrar un usuario con esa dirección de mail.",
+
+];
